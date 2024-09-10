70def6771113330a92fa75c206bd6c8f514516fa
vanvu-nguyen <nv.vu29@gmail.com>
2024-09-10 09:32:19 +0700

    before parallel setting

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -13,7 +13,7 @@ import cucumber.api.junit.Cucumber;
         monochrome = true,
         plugin = {"pretty", "html:target/site/cucumber-report-default", "json:target/site/cucumber.json"},
         snippets = SnippetType.CAMELCASE,
-        tags = {"@adminLogin"} // removed @addPosId
+        tags = {"@adminLogin, @addPosId"}
 )
 
 public class TestRunner {
